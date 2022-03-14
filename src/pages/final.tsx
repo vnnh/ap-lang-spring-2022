200d09c6ec8e5c90176aadda700f522a3d1ff49e
vnnh <90565423+vnnh@users.noreply.github.com>
2022-03-14 18:01:31 -0500

    fix positioning of logo

--- src/pages/final.tsx
+++ src/pages/final.tsx
@@ -42,19 +42,26 @@ const FinalPage = () => {
 								transition={{ duration: 2, delay: 3 }}
 							>
 								<span className="text-6xl font-jaapokki text-black">DONATE NOW</span>
-								<a className="pt-8" href="https://eji.org/">
+								<a className="pt-2" href="https://eji.org/">
 									<img src="https://eji.org/wp-content/themes/eji/_include/img/logo.svg" />
 								</a>
-								<a className="pt-8" href="https://deathpenaltyworldwide.org/">
+								<a className="pt-2" href="https://deathpenaltyworldwide.org/">
 									<img src="https://secureservercdn.net/198.71.233.33/l8z.2c6.myftpupload.com/wp-content/themes/cornell/FrontEndAssets/Home/logo_header_cornell.svg" />
 								</a>
-								<a className="pt-8" href="https://deathpenaltyaction.org/">
+								<a className="pt-2" href="https://deathpenaltyaction.org/">
 									<img src="https://deathpenaltyaction.org/wp-content/uploads/2022/02/DPA-red-and-black-on-white-WEB-HEADER-400px.png" />
 								</a>
-								<a className="pt-8" href="https://www.witnesstoinnocence.org/">
+								<a className="pt-2" href="https://www.witnesstoinnocence.org/">
 									<img src="https://static.wixstatic.com/media/434898_0380e573278542dca01b54a335f8915a~mv2.png/v1/fill/w_171,h_48,al_c,usm_0.66_1.00_0.01,enc_auto/434898_0380e573278542dca01b54a335f8915a~mv2.png" />
 								</a>
-								<a className="pt-8" href="https://exonerate.org/">
+							</motion.div>
+							<motion.div
+								className="absolute -bottom-44 flex-col items-center"
+								initial={{ opacity: 0 }}
+								animate={{ opacity: 1 }}
+								transition={{ duration: 2, delay: 3 }}
+							>
+								<a href="https://exonerate.org/">
 									<img src="https://exonerate.org/wp-content/uploads/2020/01/MAIP_logo_stacked.png" />
 								</a>
 							</motion.div>
